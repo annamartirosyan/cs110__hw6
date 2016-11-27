128b2717549fd6850f352a9fd0fcb4e3f549facf
annamartirosyan <annamartirosyan2662@gmail.com>
2016-11-27 22:45:04 +0400

    Update Math.java

--- src/com/utils/Math.java
+++ src/com/utils/Math.java
@@ -46,32 +46,3 @@ public class Math {
 
 
 
-//    public int factorial(int n){
-//        if(n>0)
-//            return n * factorial(n-1);
-//        else return 1;
-//    }
-//     public int factorialLoop(int n){
-//        int result = 1;
-//        for(n=n;n>0;n--) result = result * n;
-//        return result;
-//    }
-//}
-//    private int n;
-//    private int num;
-//
-//    public Math(int n) {
-//        this.num = n;
-//    }
-//
-//    public int Factorial(num){
-//       // int a = n.intValue();
-//        if (num == 1) {
-//            return 1;
-//        }
-//        int result = num * getFactorial(num-1);
-//        return result;
-//        }
-//
-//    }
-//}
