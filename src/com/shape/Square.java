7091c64ced390a3c21fafbd70a6b7b2850d43c5d
annamartirosyan <annamartirosyan2662@gmail.com>
2016-11-27 16:08:16 +0400

    Initial commit

---
+++ src/com/shape/Square.java
@@ -0,0 +1,12 @@
+package com.shape;
+
+public class Square{
+    private long squareWidth;
+    public Square(long a){
+        this.squareWidth = a;
+    }
+    public long getArea() {
+        long result = squareWidth*squareWidth;
+        return result;
+    }
+}
